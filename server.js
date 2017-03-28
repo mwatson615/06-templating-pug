0025bccd19f2f0f02e20535b700fc5c6f72d9315
Michael Watson <m.watson.tn@gmail.com>
2017-03-28 15:54:52 -0500

    req 4 array of objects renders correctly

--- server.js
+++ server.js
@@ -3,7 +3,34 @@
 const express = require('express');
 const app = express();
 
+
 app.set('view engine', 'pug');
+
+app.use(express.static('img'))
+
+const pastries = [
+
+	{	"name": "eclair",
+		"price": 2.99
+	},
+	{
+		"name": "donut",
+		"price": .25
+	},
+	{
+		"name": "sprinkles",
+		"price": 40.99
+	},
+	{
+		"name": "artery clogger",
+		"price": 4.99
+	},
+	{
+		"name": "slow death by chocolate",
+		"price": 7.99
+	}
+	]
+
 
 app.get('/', (req, res, next) => {
 	res.render('index', {index: 'I am an index'})
@@ -14,7 +41,7 @@ app.get('/about', (req, res, next) => {
 })
 
 app.get('/inventory', (req, res, next) => {
-	res.render('inventory', {inventory: 'I am an inventory'})
+	res.render('inventory', {pastries, loggedIn: false})
 })
 
 const port = process.env.PORT || 3000;
