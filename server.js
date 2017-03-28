c5c344484c4dd55bdc4a11aa5769c53f32f9f1e9
Michael Watson <m.watson.tn@gmail.com>
2017-03-28 14:44:50 -0500

    steps 1-3 complete

--- server.js
+++ server.js
@@ -5,19 +5,17 @@ const app = express();
 
 app.set('view engine', 'pug');
 
+app.get('/', (req, res, next) => {
+	res.render('index', {index: 'I am an index'})
+});
 
+app.get('/about', (req, res, next) => {
+	res.render('about', {about: 'I am an about'})
+})
 
-
-
-
-
-
-
-
-
-
-
-
+app.get('/inventory', (req, res, next) => {
+	res.render('inventory', {inventory: 'I am an inventory'})
+})
 
 const port = process.env.PORT || 3000;
 app.set('port', port);
